b4fc56130cd25000668843c23794b3a53f72b3c8
unknown <martin.petrov033@gmail.com>
2021-09-30 17:42:27 +0300

    Add add-playlist logic

--- music-player/pages/playlists/add/index.tsx
+++ music-player/pages/playlists/add/index.tsx
@@ -1,19 +1,31 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import { MetaTags } from "../../../components/index";
 import styles from "./AddPlaylist.module.scss";
 
 const AddPlaylist: NextPage = () => {
+  const [playlist, setPlaylist] = useState<Object>({});
+  const [title, setTitle] = useState<string>("");
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setPlaylist({ title });
+    console.log(playlist);
+  };
+
   return (
-    <div className={styles.container}>
+    <div onSubmit={(e) => handleSubmit(e)} className={styles.container}>
       <MetaTags title="Add Playlist" description="App playlist page" />
       <h2>Add Playlist</h2>
-      <form action="POST" className={styles.form}>
+      <form className={styles.form}>
         <label htmlFor="title"></label>
         <input
           type="text"
           placeholder="Title"
           id="title"
+          autoComplete="off"
           className={styles.titleField}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <br />
         <input
