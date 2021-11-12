63c9034005aed28707f5ee319e3937e877d444cb
unknown <martin.petrov033@gmail.com>
2021-11-12 07:49:10 +0200

    Improve search bar UX

--- music-player/components/search/index.tsx
+++ music-player/components/search/index.tsx
@@ -6,7 +6,8 @@ const Search = () => {
   const router = useRouter();
   const [searchedText, setSearchedText] = useState<string>("");
 
-  const search = () => {
+  const search = (e: any) => {
+    e.preventDefault();
     router.push({
       pathname: "/search",
       query: { input: searchedText },
@@ -15,18 +16,23 @@ const Search = () => {
 
   return (
     <div className={styles.searchSection}>
-      <div className={styles.icon} onClick={search}>
-        <svg id="Outline">
-          <path d="M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z" />
-        </svg>
-      </div>
-      <input
-        type="text"
-        placeholder="Search"
-        className={styles.search}
-        value={searchedText}
-        onChange={(e) => setSearchedText(e.target.value)}
-      />
+      <form onSubmit={(e) => search(e)}>
+        <label>
+          <input type="submit" className={styles.submit} />
+          <div className={styles.icon}>
+            <svg id="Outline">
+              <path d="M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z" />
+            </svg>
+          </div>
+        </label>
+        <input
+          type="text"
+          placeholder="Search"
+          className={styles.search}
+          value={searchedText}
+          onChange={(e) => setSearchedText(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
