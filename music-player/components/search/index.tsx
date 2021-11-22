0252ad718c4ce64deb49aeda0418db1f03a589d2
unknown <martin.petrov033@gmail.com>
2021-11-22 10:21:45 +0200

    Modify search component

--- music-player/components/search/index.tsx
+++ music-player/components/search/index.tsx
@@ -17,7 +17,7 @@ const Search = () => {
   return (
     <div className={styles.searchSection}>
       <form onSubmit={(e) => search(e)}>
-        <label>
+        <label htmlFor="search-box">
           <input type="submit" className={styles.submit} />
           <div className={styles.icon}>
             <svg
@@ -25,7 +25,6 @@ const Search = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
-              <title>17 search</title>
               <path d="M23.561,21.439,18.9,16.776A10.54,10.54,0,1,0,16.776,18.9l4.663,4.663a1.5,1.5,0,0,0,2.122-2.122ZM10.5,18A7.5,7.5,0,1,1,18,10.5,7.508,7.508,0,0,1,10.5,18Z" />
             </svg>
           </div>
@@ -34,6 +33,7 @@ const Search = () => {
           type="text"
           placeholder="Search"
           className={styles.search}
+          id="search-box"
           value={searchedText}
           onChange={(e) => setSearchedText(e.target.value)}
         />
